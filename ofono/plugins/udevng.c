bb5626e95a3ea58e3931b2bf976a866aa74297f3
Denis Kenzior <denkenz@gmail.com>
2023-08-01 08:21:27 -0500

    udevng: Replace g_try_new0 with g_new0
    oFono no longer uses g_try_new0 for small allocations as these
    cannot fail.

--- ofono/plugins/udevng.c
+++ ofono/plugins/udevng.c
@@ -1639,9 +1639,7 @@ static void add_serial_device(struct udev_device *dev)
 
 	modem = g_hash_table_lookup(modem_list, syspath);
 	if (modem == NULL) {
-		modem = g_try_new0(struct modem_info, 1);
-		if (modem == NULL)
-			return;
+		modem = g_new0(struct modem_info, 1);
 
 		modem->type = MODEM_TYPE_SERIAL;
 		modem->syspath = g_strdup(syspath);
@@ -1657,9 +1655,7 @@ static void add_serial_device(struct udev_device *dev)
 	DBG("%s", devpath);
 	DBG("%s (%s)", devnode, driver);
 
-	info = g_try_new0(struct serial_device_info, 1);
-	if (info == NULL)
-		return;
+	info = g_new0(struct serial_device_info, 1);
 
 	info->devpath = g_strdup(devpath);
 	info->devnode = g_strdup(devnode);
@@ -1687,9 +1683,7 @@ static void add_device(const char *syspath, const char *devname,
 
 	modem = g_hash_table_lookup(modem_list, syspath);
 	if (modem == NULL) {
-		modem = g_try_new0(struct modem_info, 1);
-		if (modem == NULL)
-			return;
+		modem = g_new0(struct modem_info, 1);
 
 		modem->type = type;
 		modem->syspath = g_strdup(syspath);
@@ -1759,9 +1753,7 @@ static void add_device(const char *syspath, const char *devname,
 	DBG("%s (%s) %s [%s] ==> %s %s", devnode, driver,
 					interface, number, label, sysattr);
 
-	info = g_try_new0(struct device_info, 1);
-	if (info == NULL)
-		return;
+	info = g_new0(struct device_info, 1);
 
 	info->devpath = g_strdup(devpath);
 	info->devnode = g_strdup(devnode);
